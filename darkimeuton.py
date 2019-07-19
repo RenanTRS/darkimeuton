ff5f104ac5e4d4d73b319e75fcc884cd49155ad3
RenanTRS <renantrsofficial@gmail.com>
2019-07-19 12:33:30 -0400

    Acréscimo de letras

--- darkimeuton.py
+++ darkimeuton.py
@@ -6,14 +6,20 @@ p = []
 for c in range(0, qtd):
     if palavra[c] == 'A':
         p.append(4)
+    elif palavra[c] == 'G':
+        p.append(6)
     elif palavra[c] == 'E':
         p.append(3)
     elif palavra[c] == 'I':
         p.append(1)
     elif palavra[c] == 'O':
         p.append(0)
+    elif palavra[c] == 'S':
+        p.append(5)
     elif palavra[c] == 'T':
         p.append(7)
+    elif palavra[c] == 'Z':
+        p.append(2)
     else:
         p.append(palavra[c])
 
